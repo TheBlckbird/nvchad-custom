ff107b08c9c8719676885fee197f7317c0524bbe
Louis Weigel <account@louisweigel.com>
2023-08-15 16:30:06 +0200

    Add Laravel plugin

--- plugins.lua
+++ plugins.lua
@@ -73,6 +73,44 @@ local plugins = {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      {
+        "<leader>lt",
+        function()
+          require("laravel.tinker").send_to_tinker()
+        end,
+        mode = "v",
+        desc = "Laravel Application Routes",
+      },
+    },
+    event = { "VeryLazy" },
+    config = function()
+      require("laravel").setup()
+      require("telescope").load_extension "laravel"
+    end,
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local notify = require "notify"
+      -- this for transparency
+      notify.setup { background_colour = "#000000" }
+      -- this overwrites the vim notify function
+      vim.notify = notify.notify
+    end,
+  },
 }
 
 return plugins
