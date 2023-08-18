032b72056a0da6df21a6f5a4d96141821b533be8
Louis Weigel <account@louisweigel.com>
2023-08-18 22:06:21 +0200

    Remove whitespace

--- plugins.lua
+++ plugins.lua
@@ -1,37 +1,20 @@
 return {
   require "custom.plugins.copilot",
-
   require "custom.plugins.lspconfig",
-
   require "custom.plugins.lspconfig",
-
   require "custom.plugins.todo-comments",
+  require "custom.plugins.php-refactoring",
+  require "custom.plugins.discord-presence",
+  require "custom.plugins.bible-votd",
+  require "custom.plugins.oil",
+  require "custom.plugins.vim-tmux-navigator",
+  require "custom.plugins.laravel",
+  require "custom.plugins.nvim-notify",
+  require "custom.plugins.svelte",
+  require "custom.plugins.spectre",
+  require "custom.plugins.nvim-ts-autotag",
 
   require "custom.plugins.overrides.nvim-tree",
-
-  require "custom.plugins.overrides.nvim-colorizer",
-
-  require "custom.plugins.php-refactoring",
-
-  require "custom.plugins.discord-presence",
-
-  require "custom.plugins.bible-votd",
-
-  require "custom.plugins.oil",
-
-  require "custom.plugins.vim-tmux-navigator",
-
-  require "custom.plugins.laravel",
-
-  require "custom.plugins.nvim-notify",
-
-  require "custom.plugins.colortils",
-
-  require "custom.plugins.svelte",
-
   require "custom.plugins.overrides.nvim-cmp",
-
-  require "custom.plugins.spectre",
-
-  require "custom.plugins.nvim-ts-autotag",
+  require "custom.plugins.overrides.nvim-colorizer",
 }
